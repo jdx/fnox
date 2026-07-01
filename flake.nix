c6587984f97de592db3e7da5962f761fcce2c069
o-az <23618431+o-az@users.noreply.github.com>
2026-07-01 05:38:43 -0700

    feat: flake-utils

--- flake.nix
+++ flake.nix
@@ -3,85 +3,82 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
-      self,
       crane,
       nixpkgs,
+      flake-utils,
+      ...
     }:
-    let
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          craneLib = crane.mkLib pkgs;
-          src = pkgs.lib.cleanSourceWith {
-            src = craneLib.path ./.;
-            filter =
-              path: type:
-              (craneLib.filterCargoSources path type) || (pkgs.lib.hasInfix "/src/assets/" (toString path));
-          };
-          commonArgs = {
-            inherit src;
-            strictDeps = true;
-            nativeBuildInputs = with pkgs; [
-              perl
-              pkg-config
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+        src = pkgs.lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter =
+            path: type:
+            (craneLib.filterCargoSources path type) || (pkgs.lib.hasInfix "/src/assets/" (toString path));
+        };
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          nativeBuildInputs = with pkgs; [
+            perl
+            pkg-config
+          ];
+          buildInputs =
+            with pkgs;
+            [ ]
+            ++ lib.optionals stdenv.isLinux [
+              dbus
+              udev
             ];
-            buildInputs =
-              with pkgs;
-              [
-              ]
-              ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
-                dbus
-                udev
-              ];
-
-          };
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-          fnox =
-            let
-              cargoToml = craneLib.crateNameFromCargoToml { inherit src; };
-            in
-            craneLib.buildPackage (
-              commonArgs
-              // {
-                inherit cargoArtifacts;
-                doCheck = false;
-                meta = {
-                  description = "A flexible secret management tool by @jdx";
-                  homepage = "https://github.com/jdx/fnox";
-                  changelog = "https://github.com/jdx/fnox/releases/tag/v${cargoToml.version}";
-                  license = pkgs.lib.licenses.mit;
-                  mainProgram = "fnox";
-                  platforms = systems;
-                };
-              }
-            );
-        in
-        {
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        fnox =
+          let
+            cargoToml = craneLib.crateNameFromCargoToml { inherit src; };
+          in
+          craneLib.buildPackage (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              doCheck = false;
+              meta = {
+                mainProgram = "fnox";
+                description = "A flexible secret management tool by @jdx";
+                homepage = "https://github.com/jdx/fnox";
+                changelog = "https://github.com/jdx/fnox/releases/tag/v${cargoToml.version}";
+                license = pkgs.lib.licenses.mit;
+                platforms = flake-utils.lib.defaultSystems;
+              };
+            }
+          );
+      in
+      {
+        packages = {
           default = fnox;
           fnox = fnox;
-        }
-      );
-
-      apps = forAllSystems (system: {
-        default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/fnox";
         };
-      });
-    };
+
+        apps.default = {
+          type = "app";
+          program = "${fnox}/bin/fnox";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            clippy
+            rustfmt
+          ];
+        };
+      }
+    );
 }
